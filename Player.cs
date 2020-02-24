6ddbde0edb414a061e0fa676e05948ef258d0f02
Elise Navarro <elise.marie.navarro@gmail.com>
2020-02-24 11:18:17 -0700

    Initialize Player.cs class and methods

---
+++ Player.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+class Player // set the player
+{
+
+  // Get total value of cards in hand
+  public int GetTotal()
+  {
+
+  }
+
+  // check if player has Blackjack
+  public bool HasBlackjack()
+  {
+
+  }
+
+  // Check is player/dealer hand is over 21
+  public bool CheckOverTwentyOne()
+  {
+
+  }
+
+  // Ask player to Hold or Draw another card
+  public virtual void Hold()
+  {
+
+  }
+
+  // Allow player to view hand
+  public virtual void ViewHand()
+  {
+
+  }
+
+  // Update the value of Ace card
+  public void UpdateAces()
+  {
+
+  }
+}
